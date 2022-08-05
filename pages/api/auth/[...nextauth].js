81d6449a9bd94b587e322e67a0d8343a562da0a5
gefgu <gefgu@hotmail.com>
2022-08-05 15:13:51 -0300

    Add facebook login functionality

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,9 @@ import FacebookProvider from "next-auth/providers/facebook";
 export default NextAuth({
   providers: [
     FacebookProvider({
-      clientId: process.env.clientId,
-      clientSecret: process.env.clientId,
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
     }),
   ],
+  debug: false,
 });
