1c1898c107f8a4656a956b8dbc11178a67585fcd
gefgu <gefgu@hotmail.com>
2022-08-05 16:45:58 -0300

    Add sign up functionality for auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,26 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth/next";
 import FacebookProvider from "next-auth/providers/facebook";
+import clientPromise from "../../../lib/mongodb";
 
 export default NextAuth({
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
+      profile(profile) {
+        console.log(profile);
+        return {
+          name: profile.name,
+          image: profile.picture.data.url,
+          friends: [],
+          friendshipRequests: [],
+          id: profile.id,
+          email: profile.email,
+        };
+      },
     }),
   ],
   debug: false,
+  adapter: MongoDBAdapter(clientPromise),
 });
