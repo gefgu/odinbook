6aa1b49984c6aed0bf5e42416113716378d6db32
gefgu <gefgu@hotmail.com>
2022-08-05 15:59:32 -0300

    Add query of all users in api

---
+++ pages/api/users.js
@@ -0,0 +1,10 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db(process.env.MONGODB_DB);
+
+  const users = await db.collection("users").find({}).toArray();
+
+  res.status(200).json({ users });
+}
