502cb9d556c2f355ea7b8aa687e450e3bdbea7d3
gefgu <gefgu@hotmail.com>
2022-08-07 10:43:34 -0300

    Add GET of user posts

---
+++ pages/api/users/[userId]/posts.js
@@ -0,0 +1,13 @@
+import { ObjectId } from "mongodb";
+import clientPromise from "../../../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db(process.env.MONGODB_DB);
+
+  const { userId } = req.query;
+
+  const users = await db.collection("posts").find({ author: new ObjectId(userId) }).toArray();
+
+  res.status(200).json({ users });
+}
