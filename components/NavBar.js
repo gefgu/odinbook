60a7ab8650a0ff7e30bc5d753ed0d0a250a5e5ed
gefgu <gefgu@hotmail.com>
2022-08-08 16:32:10 -0300

    Add navbar styling

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,28 +3,36 @@ import odin from "../public/odin.png";
 import home from "../public/home.png";
 import people from "../public/account-multiple.png";
 import { useSession } from "next-auth/react";
+import styles from "../styles/navbar.module.css";
 
 export default function NavBar() {
   const { data: session } = useSession();
 
   return (
-    <header>
-      <Image src={odin} alt="Odin Logo" width={50} height={50} />
+    <header className={styles.container}>
+      <Image src={odin} alt="Odin Logo" layout="fixed" height={50} width={50} />
       <nav>
-        <ul>
+        <ul className={styles.list}>
           <li>
-            <button>
-              <Image src={home} alt="Home Page" width={50} height={50} />
+            <button className={styles.active}>
+              <Image src={home} alt="Home Page" width={40} height={40} />
             </button>
           </li>
           <li>
             <button>
-              <Image src={people} alt="Friends" width={50} height={50} />
+              <Image src={people} alt="Friends" width={40} height={40} />
             </button>
           </li>
         </ul>
       </nav>
-      <Image src={session?.user.image} alt="Profile" width={50} height={50} />
+      <Image
+        src={session?.user.image}
+        className={styles.rounded}
+        alt="Profile"
+        layout="fixed"
+        width={50}
+        height={50}
+      />
     </header>
   );
 }
