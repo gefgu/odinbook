2da90040debe9cce03b2e7376bbfd2ca5cb85392
gefgu <gefgu@hotmail.com>
2022-08-08 15:06:41 -0300

    Add styling for signin page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,17 +1,25 @@
 import { getProviders, signIn } from "next-auth/react";
 import HeadData from "../../components/HeadData";
+import styles from "../../styles/signin.module.css";
 
 export default function SignIn({ providers }) {
   return (
     <>
       <HeadData />
-      <main>
-        <h1>Odinbook</h1>
-        <h2>Connect with friends and the world around you on Odinbook</h2>
-        <div>
+      <main className={styles.container}>
+        <div className={styles.text}>
+          <h1 className={styles.title}>Odinbook</h1>
+          <h2 className={styles.cta}>
+            Connect with friends and the world around you on Odinbook
+          </h2>
+        </div>
+        <div className={styles.box}>
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
-              <button onClick={() => signIn(provider.id)}>
+              <button
+                className={styles.button}
+                onClick={() => signIn(provider.id)}
+              >
                 Sign in with {provider.name}
               </button>
             </div>
