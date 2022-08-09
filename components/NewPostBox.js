3ad63e35ef4175909586ddf20a61b1c1a9db0c07
gefgu <gefgu@hotmail.com>
2022-08-09 14:58:18 -0300

    Add styling for new post box

--- components/NewPostBox.js
+++ components/NewPostBox.js
@@ -1,28 +1,34 @@
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import styles from "../styles/navbar.module.css";
+import styles from "../styles/NewPostBox.module.css";
 
 export default function NavBar() {
   const { data: session } = useSession();
 
   return (
-    <div>
-      <Image
-        src={session?.user.image}
-        className={styles.rounded}
-        alt="Profile"
-        layout="fixed"
-        width={50}
-        height={50}
-      />
-      <form>
-        <textarea
-          name="content"
-          placeholder={`What's on your mind, ${
-            session?.user.name.split(" ")[0]
-          }?`}
-        ></textarea>
-        <button>Submit</button>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <div className={styles.flex}>
+          <Image
+            src={session?.user.image}
+            className={styles.rounded}
+            alt="Profile"
+            layout="fixed"
+            width={50}
+            height={50}
+          />
+          <textarea
+            name="content"
+            placeholder={`What's on your mind, ${
+              session?.user.name.split(" ")[0]
+            }?`}
+            className={styles.textarea}
+            rows={4}
+          ></textarea>
+        </div>
+        <div className={styles.right}>
+          <button className={styles.button}>Submit</button>
+        </div>
       </form>
     </div>
   );
