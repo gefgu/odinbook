d119eedba0e4f2ae68135c4b6c3f5428525050d8
gefgu <gefgu@hotmail.com>
2022-08-10 16:08:34 -0300

    Add like box styling

--- components/LikeBox.js
+++ components/LikeBox.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import thumbs from "../public/thumbs-up-solid.svg";
+import styles from "../styles/LikeBox.module.css";
 
 export default function LikeBox() {
   const { data: session } = useSession();
@@ -11,10 +12,10 @@ export default function LikeBox() {
 
   return (
     session && (
-      <div>
+      <div className={styles.container}>
         <form onSubmit={handleSubmit}>
           <button>
-            <Image src={thumbs} alt="Thumbs up" width={50} /> Like
+            <Image src={thumbs} alt="Thumbs up" width={30} height={30} /> Like
           </button>
         </form>
       </div>
