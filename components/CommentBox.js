f36f32399ccd6e4151574e40e8ba5159a170b7ff
gefgu <gefgu@hotmail.com>
2022-08-11 16:11:36 -0300

    Add styling for comments display

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import useSWR from "swr";
 import NewCommentBox from "./NewCommentBox";
 import utils from "../styles/utils.module.css";
+import styles from "../styles/CommentBox.module.css";
+import { getSinceDateUntilNow } from "../lib/helpers";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -19,25 +21,30 @@ export default function CommentBox({ post }) {
   console.log(comments);
 
   return (
-    <div>
+    <div className={styles.container}>
       {comments &&
-        comments.map((comment) => (
-          <div key={comment._id}>
-            <Image
-              src={comment.author.image}
-              className={utils.rounded}
-              alt="Profile"
-              layout="fixed"
-              width={50}
-              height={50}
-            />
-            <div>
-              <strong>{comment.author.name}</strong>
-              <p>{comment.content}</p>
+        comments.map((comment) => {
+          comment.sinceCreation = getSinceDateUntilNow(comment.creationDate);
+          return (
+            <div key={comment._id} className={styles.commentBox}>
+              <Image
+                src={comment.author.image}
+                className={utils.rounded}
+                alt="Profile"
+                layout="fixed"
+                width={50}
+                height={50}
+              />
+              <div className={styles.comment}>
+                <div className={styles.content}>
+                  <strong>{comment.author.name}</strong>
+                  <p>{comment.content}</p>
+                </div>
+                <p className={styles.date}>{comment.sinceCreation}</p>
+              </div>
             </div>
-            <p>{comment.sinceCreation}</p>
-          </div>
-        ))}
+          );
+        })}
       <NewCommentBox update={mutate} post={post} />
     </div>
   );
